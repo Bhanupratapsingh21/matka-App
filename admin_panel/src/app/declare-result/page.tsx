f94fdb73916ef8ddef6a87535498008f05877f4b
Khushi Rathore <khushirathore649@gmail.com>
2025-07-17 15:40:33 +0530

    resolved conflicts

--- admin_panel/src/app/declare-result/page.tsx
+++ admin_panel/src/app/declare-result/page.tsx
@@ -1,12 +1,4 @@
-<<<<<<< HEAD
-const declareResults = () => {
-    return (<>
-        declare results page
-    </>)
-}
 
-export default declareResults;
-=======
 import DeclareResultContent from "@/components/declareResultContent";
 
 export default function DeclareResultPage() {
@@ -16,4 +8,4 @@ export default function DeclareResultPage() {
     </main>
   );
 }
->>>>>>> f9f3d5f5d96ec92700c83ddb0527ac3e9047b0f3
+
